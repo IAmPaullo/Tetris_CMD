a8c44eb48a80ed0104a619e96593e5b9142d08a8
Paullo B Felipe <iampaullo@live.com>
2022-02-10 00:55:33 -0300

    Add project files.

---
+++ Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris_CMD
+{
+    class Game
+    {
+
+
+        public void Start()
+        {
+
+        }
+    }
+
+
+   
+}
