889a18370edfcfce6ff24bdb8bf3489cc6ceb0a6
Paullo B Felipe <iampaullo@live.com>
2022-02-10 02:02:21 -0300

    Criando funções futuras

--- GameBoard.cs
+++ GameBoard.cs
@@ -11,15 +11,80 @@ namespace Tetris_CMD
         public const int boardHeight = 40;
         public const int boardWidth = 20;
 
+        private int linesPerLvlAmount = 10;
+        private int shapeAmount = 7;
+        private int lines = 0;
+        private int score = 0;
+        private int level = 0;
+        private float dropSpeed;
+        private bool isFinished = false;
+        private Grid levelGrid;
+        private ObjectShape currentShape, nextShape;
+
+
         public GameBoard()
         {
+
+        }
+        public bool GameHasFinished()
+        {
+            return isFinished;
+        }
+
+
+
+        public void GetKeyInput()
+        {
+            ConsoleKey consoleKey;
+
+            if (Console.KeyAvailable)
+            {
+                consoleKey = Console.ReadKey().Key;
+
+                switch (consoleKey)
+                {
+                    default:
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+                        //mover pra esquerda
+                        //MoveLeft(); em algum momento
+                        break;
+
+                    case ConsoleKey.RightArrow:
+
+                        break;
+
+                    case ConsoleKey.DownArrow:
+
+                        break;
+
+                    case ConsoleKey.Spacebar:
+
+                        break;
+                }
+            }
 
         }
 
         public void DrawBoard()
         {
-            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.Clear();
             Console.ForegroundColor = ConsoleColor.Blue;
+            //levelGrid.Draw();
+
         }
+
+        private void DrawBorder()
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            //Desenhar uma caixa aqui?
+        }
+
+        public void UpdateGameLoop()
+        {
+
+        }
+
     }
 }
